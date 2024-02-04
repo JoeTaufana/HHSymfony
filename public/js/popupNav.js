c0ff34b6b2f6d8ac989272e318a8c087910598a8
ANAWARdev <143820904+ANAWARdev@users.noreply.github.com>
2024-02-04 14:39:17 +0100

    changes

---
+++ public/js/popupNav.js
@@ -0,0 +1,8 @@
+function afficherPopup() {
+  document.getElementById("popupNav").style.display = "block";
+}
+
+function fermer() {
+  document.getElementById("popupNav").style.display = "none";
+}
+
