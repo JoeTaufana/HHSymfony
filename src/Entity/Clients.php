e5d81ce039cfbf149bcc38ad0358a3214cdb5fe2
JoeTaufana <taufanajoe@gmail.com>
2024-02-05 14:45:40 +0100

    création entité clients

---
+++ src/Entity/Clients.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientsRepository::class)]
+class Clients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photoProfil = null;
+
+    #[ORM\Column]
+    private ?bool $optInSms = null;
+
+    #[ORM\Column]
+    private ?bool $optInMail = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateNaissance = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nombreEnfant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getPhotoProfil(): ?string
+    {
+        return $this->photoProfil;
+    }
+
+    public function setPhotoProfil(?string $photoProfil): static
+    {
+        $this->photoProfil = $photoProfil;
+
+        return $this;
+    }
+
+    public function isOptInSms(): ?bool
+    {
+        return $this->optInSms;
+    }
+
+    public function setOptInSms(bool $optInSms): static
+    {
+        $this->optInSms = $optInSms;
+
+        return $this;
+    }
+
+    public function isOptInMail(): ?bool
+    {
+        return $this->optInMail;
+    }
+
+    public function setOptInMail(bool $optInMail): static
+    {
+        $this->optInMail = $optInMail;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(?\DateTimeInterface $dateNaissance): static
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getNombreEnfant(): ?int
+    {
+        return $this->nombreEnfant;
+    }
+
+    public function setNombreEnfant(?int $nombreEnfant): static
+    {
+        $this->nombreEnfant = $nombreEnfant;
+
+        return $this;
+    }
+}
