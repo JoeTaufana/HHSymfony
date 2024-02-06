95307a247c2c5473a8d3a63cea97ef17de89350c
ALouKatraz <louise.casteleyn@gmail.com>
2024-02-06 14:12:49 +0100

    Création d'une nouvelle fonction dans document JS. Modification de la base HTML pour obtenir une popup lors de l'appuie sur la photo de profil et momdification du CSS en conséquence

--- public/js/popupNav.js
+++ public/js/popupNav.js
@@ -21,3 +21,28 @@ if (btnMenu) {
   // Ajout d'un écouteur d'événements au clic sur le bouton, appelant la fonction menu définie
   btnMenu.addEventListener("click", menu);
 }
+
+
+//Création d'une fonction nommée menu2 pour appuyer sur la photo
+function menu2() {
+  // Sélection de l'élément avec la class "popupNav"
+  var popupPhoto = document.getElementById("popupPhoto");
+
+  // Vérification du style actuel de l'élément
+  if (popupPhoto.style.display === "block") {
+    // Si le style est "block", masquer l'élément en définissant le style sur "none"
+    popupPhoto.style.display = "none";
+  } else {
+    // Sinon, afficher l'élément en définissant le style sur "block"
+    popupNav.style.display = "block";
+  }
+}
+
+// Sélection du bouton avec la classe "btnPhoto" dans le document
+var btnPhoto = document.getElementById("photo_profil");
+
+// Vérification de l'existence du bouton
+if (btnPhoto) {
+  // Ajout d'un écouteur d'événements au clic sur le bouton, appelant la fonction menu définie
+  btnPhoto.addEventListener("click", menu2);
+}
