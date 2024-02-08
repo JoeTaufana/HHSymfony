712b7e0c244a47ea48554b06f0577ff73dd7f721
ANAWARdev <143820904+ANAWARdev@users.noreply.github.com>
2024-02-08 12:00:08 +0100

    récupération de mon travail

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,10 +1,12 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -26,4 +28,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
