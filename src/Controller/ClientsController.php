30ad9ebe05eb04e42c3f90a012f2397cd9ac7e24
JoeTaufana <taufanajoe@gmail.com>
2024-02-09 12:18:56 +0100

    MAJ clients

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Clients;
-use App\Entity\User;
-use App\Form\RegistrationFormClientsType;
+
 use App\Form\ClientsType;
-use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,56 +12,104 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+
+
 class ClientsController extends AbstractController
 {
-
-    /**
+    /** 
      * @Route("/clients", name="app_clients")
-     */
-    function clients(Request $request, EntityManagerInterface $entityManager): Response
+     **/
+    public function clients(Request $request, EntityManagerInterface $entityManager): Response
     {
+        echo '<pre>';
+        print_r($this->getUser());
+        echo '</pre>';
+        //Vérifie si l'utilisateur est connecté
         if ($this->getUser()) {
             $client = new Clients();
+            echo '<pre>';
+            print_r($client->getId());
+            echo '</pre>';
             $form = $this->createForm(ClientsType::class, $client);
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
-                // il faut lier le user et le client via la clé commune
+                $client = $form->getData();
                 $client->setUser($this->getUser());
                 $entityManager->persist($client);
                 $entityManager->flush();
-                return $this->render('index/index.html.twig', [
+                return $this->redirectToRoute('app_index');
+            }
+            //Vérifie si $client est une instance de la classe Clients et si elle a une propriété "id"
+            if ($client instanceof Clients && method_exists($client, 'getId')) {
+                //Transmet $client à votre template
+                return $this->render('clients/index.html.twig', [
+                    'client' => $client,
                     'Clients' => $form->createView(),
                 ]);
+            } else {
+                //Si l'utilisateur n'est pas connecté, redirige vers la page de connexion
+                return $this->redirectToRoute('login');
             }
-            
-            return $this->render('clients/index.html.twig', [
-                'Clients' => $form->createView(),
+        }
+    }
+
+
+    /**
+     * @Route("/clients/{id}/edit", name="app_editClient")
+     */
+
+    public function editClient(Request $request, EntityManagerInterface $entityManager, Clients $client): Response
+    {
+
+        // Vérifie si l'utilisateur est connecté et est le propriétaire du client
+        if ($this->getUser() && $client->getUser() === $this->getUser()) {
+            // Crée le formulaire avec le type ClientsType et l'entité client existant
+            $form = $this->createForm(ClientsType::class, $client);
+            $form->handleRequest($request);
+
+            // Vérifie si le formulaire a été soumis et est valide
+            if ($form->isSubmitted() && $form->isValid()) {
+                // Enregistre les modifications dans la base de données
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($client);
+                $entityManager->flush();
+
+                // Redirige vers cette même page après modification
+                return $this->redirectToRoute('app_editClient', ['id' => $client->id]);
+            }
+
+            return $this->render('clients/edit.html.twig', [
+                'clients' => $form->createView(),
             ]);
         } else {
-            // non logué il faut le rediriger vers la page login
+            // Si l'utilisateur n'est pas connecté ou n'est pas le propriétaire du client, redirige vers la page de connexion
             return $this->redirectToRoute('login');
-        }   
+        }
     }
-    // public function editerClient(Request $request, Clients $client): Response
-    // {
-    //     $formulaire = $this->createForm(ClientsType::class, $client);
 
-    //     $formulaire->handleRequest($request);
 
-    //     if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-    //         // Mettez à jour l'entité Client avec les données du formulaire
-    //         $this->getDoctrine()->getManager()->flush();
 
-    //         $this->addFlash('success', 'Les informations du client ont été mises à jour avec succès.');
 
-    //         return $this->redirectToRoute('votre_route_pour_afficher_les_clients');
-    //     }
 
-    //     return $this->render('client/editer.html.twig', [
-    //         'formulaire' => $formulaire->createView(),
-    //     ]);
-    // }
+    //         //Supprime un utilisateur associé à un client.
+//         /**
+//          * @Route("/clients/{id}/delete", name="deleteClient")
+//          */
+//         public function deleteClient(EntityManagerInterface $entityManager, Clients $client): Response
+//         {
+//             // Vérifie si l'utilisateur est connecté et est le propriétaire du client
+//             if ($this->getUser() && $client->getUser() === $this->getUser()) {
+//                 // Supprime l'utilisateur associé au client (cela dépend de votre configuration de relation)
+//                 $entityManager->remove($client);
+//                 $entityManager->flush();
+
+    //                 return $this->redirectToRoute('app_index'); // Redirige vers la page d'accueil du site
+//             } else {
+//                 // Si l'utilisateur n'est pas connecté ou n'est pas le propriétaire du client, redirige vers la page de connexion
+//                 return $this->redirectToRoute('login');
+//             }
+//         }
+
+    // 
 }
-
-
